28a1fbe610e5f37a5970fce66aa4e053d1e9713e
KUNMEI <kmei3@hawk.iit.edu>
2015-04-02 13:24:08 -0500

    111
    111

--- Thesis/Code/subcode/Untitled7.m
+++
@@ -1,5 +0,0 @@
-[Inhal, ~, ~] = xlsread('EFHcalculation_MK','Inhalation');
-[surface, ~, ~] = xlsread('EFHcalculation_MK','Dermal');
-[frequency, ~, ~] = xlsread('EFHcalculation_Mk','nondietary');
-
-save('C:\Documents and Settings\kunmei\My Documents\Thesis\Code\subcode\EFHdata.mat','Inhal','surface','frequency');
